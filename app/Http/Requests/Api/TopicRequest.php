0a60e76a99c35d370b73893eac9b0b7d11be0e2e
marsccyu <marsccyu@gmail.com>
2019-06-14 15:23:25 +0800

    合併檔案

---
+++ app/Http/Requests/Api/TopicRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TopicRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        switch($this->method()) {
+            case 'POST':
+                return [
+                    'title' => 'required|string',
+                    'body' => 'required|string',
+                    'category_id' => 'required|exists:categories,id',
+                ];
+                break;
+            case 'PATCH':
+                return [
+                    'title' => 'string',
+                    'body' => 'string',
+                    'category_id' => 'exists:categories,id',
+                ];
+                break;
+        }
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => '標題',
+            'body' => '文章內容',
+            'category_id' => '分類',
+        ];
+    }
+}
